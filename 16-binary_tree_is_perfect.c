5b1880cfe855ef897f4820e8c6351b8e883bbe93
fabo893 <jose.rosa893@gmail.com>
2021-03-10 15:34:39 +0000

    adding a return

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -61,9 +61,12 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	{
 		left = binary_tree_size(tree->left);
 		right = binary_tree_size(tree->right);
-
-		if (left == right)
-			return (1);
 	}
+	else
+		return (0);
+
+	if (left == right)
+		return (1);
+
 	return (0);
 }
