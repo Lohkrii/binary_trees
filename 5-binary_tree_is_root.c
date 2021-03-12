50608cb03a48f24df1ee44844c105f98e2df6539
Lohkrii <joshualopez0429@gmail.com>
2021-03-12 02:23:22 +0000

    5-binary_tree_is_root.c: Added '()' around comparitor

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -8,10 +8,10 @@
 
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	if (node == NULL)
+	if (!node)
 		return (0);
 
-	if (!node->parent)
+	if (!(node->parent))
 		return (1);
 	return (0);
 }
