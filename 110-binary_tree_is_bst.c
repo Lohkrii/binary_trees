a16d9b12305adbfd39e417f3932e785da2d277a0
Lohkrii <joshualopez0429@gmail.com>
2021-03-12 02:04:28 +0000

    110-binary_tree_is_bst.c: Verify if tree is BST

---
+++ 110-binary_tree_is_bst.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+
+/**
+ * bst_helper - Actually compares to find if tree is bst.
+ * @tree: Is the tree to look into.
+ * Return: Returns the minumum value of the tree.
+ */
+
+int bst_helper(const binary_tree_t *tree, int min, int max)
+{
+	if (!tree)
+		return (1);
+	if (tree->n < min || tree->n > max)
+		return (0);
+	return (bst_helper(tree->left, min, tree->n - 1)
+			&& bst_helper(tree->right, tree->n + 1, max));
+}
+
+/**
+ * binary_tree_is_bst - Verifys the given tree is a BST.
+ * @tree: Pointer to the root of the tree in question.
+ * Return: If bst return 1, otherwise return 0.
+ */
+
+int binary_tree_is_bst(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (bst_helper(tree, INT_MIN, INT_MAX));
+}
